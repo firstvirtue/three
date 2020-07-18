5875aea27a875aeefb173f5bdd29831205865602
Jake Lee <firstvirtue@gmail.com>
2020-07-18 20:37:13 +0900

    Add first gltf example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // import model from '/assets/models/ocean.gltf';
 
 function component() {
@@ -10,38 +11,102 @@ function component() {
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  scene.background = new THREE.Color('black');
+  var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 100 );
 
   var renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
+  document.body.appendChild(renderer.domElement);
 
-  var geometry = new THREE.BoxGeometry();
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
-  var cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+  camera.position.set(0, 10, 20);
 
-  camera.position.z = 5;
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 5, 0);
+  controls.update();
 
-  var light = new THREE.PointLight( 0xff0000, 1, 100 );
-  light.position.set( 50, 50, 50 );
-  scene.add( light );
+  {
+    const skyColor = 0xB1E1FF;  // light blue
+    const groundColor = 0xB97A20;  // brownish orange
+    const intensity = 1;
+    const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+    scene.add(light);
+  }
 
+  {
+    var light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(5, 10, 2);
+    scene.add(light);
+    scene.add(light.target); //?
+  }
+
+  function frameArea(sizeToFitOnScreen, boxSize, boxCenter, camera) {
+    const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5;
+    const halfFovY = THREE.MathUtils.degToRad(camera.fov * .5);
+    const distance = halfSizeToFitOnScreen / Math.tan(halfFovY);
+    // compute a unit vector that points in the direction the camera is now
+    // in the xz plane from the center of the box
+    const direction = (new THREE.Vector3())
+        .subVectors(camera.position, boxCenter)
+        .multiply(new THREE.Vector3(1, 0, 1))
+        .normalize();
+
+    // move the camera to a position distance units way from the center
+    // in whatever direction the camera was from the center already
+    camera.position.copy(direction.multiplyScalar(distance).add(boxCenter));
+
+    // pick some near and far values for the frustum that
+    // will contain the box.
+    camera.near = boxSize / 100;
+    camera.far = boxSize * 100;
+
+    camera.updateProjectionMatrix();
+
+    // point the camera to look at the center of the box
+    camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
+  }
+
+  function dumpObject(obj, lines = [], isLast = true, prefix = '') {
+    const localPrefix = isLast ? '└─' : '├─';
+    lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
+    const newPrefix = prefix + (isLast ? '  ' : '│ ');
+    const lastNdx = obj.children.length - 1;
+    obj.children.forEach((child, ndx) => {
+      const isLast = ndx === lastNdx;
+      dumpObject(child, lines, isLast, newPrefix);
+    });
+    return lines;
+  }
+
+  let cars;
   const loader = new GLTFLoader();
   loader.load(
     // resource URL
-    '/public/models/cup.glb',
+    // '/public/models/cup.glb',
+    '/public/models/scene.gltf',
     // called when the resource is loaded
-    function ( gltf ) {
-  
-      scene.add( gltf.scene );
+    function (gltf) {
+      
+      const root = gltf.scene;
+      scene.add(root);
+      console.log(dumpObject(root).join('\n'));
+      cars = root.getObjectByName('Cars');
   
       gltf.animations; // Array<THREE.AnimationClip>
       gltf.scene; // THREE.Group
       gltf.scenes; // Array<THREE.Group>
       gltf.cameras; // Array<THREE.Camera>
       gltf.asset; // Object
-  
+
+      const box = new THREE.Box3().setFromObject(root);
+      const boxSize = box.getSize(new THREE.Vector3()).length();
+      const boxCenter = box.getCenter(new THREE.Vector3());
+
+      frameArea(boxSize * 0.5, boxSize, boxCenter, camera);
+
+      // update the Trackball controls to handle the new size
+      controls.maxDistance = boxSize * 10;
+      controls.target.copy(boxCenter);
+      controls.update();
     },
     // called while loading is progressing
     function ( xhr ) {
@@ -57,13 +122,19 @@ function component() {
     }
   );
 
-  function animate() {
-    requestAnimationFrame( animate );
-    
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+  function animate(time) {
+    time *= 0.001;
+    // console.log(time);
+
+    if(cars) {
+      for (const car of cars.children) {
+        car.rotation.y = time;
+      }
+    }
 
     renderer.render( scene, camera );
+
+    requestAnimationFrame(animate);
   }
   animate();
 
